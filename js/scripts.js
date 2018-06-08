28e901e1e06bdd69be392b7fa6fd9d083688ea21
Eddie Harris <harrisejm@gmail.com>
2018-06-08 10:28:05 -0700

    add jquery to output results

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,47 @@
-var numb = 12;
-//var stringLength = numb.toString().length;
+$(document).ready(function() {
+  $("form#inputNumber").submit(function(event) {
+    event.preventDefault();
 
 
-var numbArr = [];
+  var numb = parseInt($("input#number").val());
+  var numbArr = [];
 
-for (var i = 0; i < numb+1; i++) {
-  numbArr.push(i.toString());
-}
-
-var nestedArr = [];
-
-for (var a = 0; a < numbArr.length; a++) {
-  var singleNumb = numbArr[a];
-  var singleNumbArr = singleNumb.split("");
-  nestedArr.push(singleNumbArr);
-}
-
-var test = [];
-var tests = [];
-
-for (var c = 0; c < nestedArr.length; c++){
-
- if (parseInt(nestedArr[c]) % 3 === 0 && parseInt(nestedArr[c]) !== 0) {
-    test.push("sorry");
-    tests += "sorry" + "</br>"
- } else if (nestedArr[c].includes('1')) {
-    test.push("boop");
-    tests += "boop" + "</br>"
-
- } else if (nestedArr[c].includes('0')) {
-    test.push("beep");
-    tests += "beep" + "</br>"
-  } else {
-    test.push(nestedArr[c].join(""));
-    tests += nestedArr[c].join("") + "</br>"
+  for (var i = 0; i < numb+1; i++) {
+    numbArr.push(i.toString());
   }
- }
+
+  var nestedArr = [];
+
+  for (var a = 0; a < numbArr.length; a++) {
+    var singleNumb = numbArr[a];
+    var singleNumbArr = singleNumb.split("");
+    nestedArr.push(singleNumbArr);
+  }
+
+  var test = [];
+  var tests = "";
+
+  for (var c = 0; c < nestedArr.length; c++){
+
+   if (parseInt(nestedArr[c]) % 3 === 0 && parseInt(nestedArr[c]) !== 0) {
+      test.push("sorry");
+      tests += "sorry" + "</br>"
+   } else if (nestedArr[c].includes('1')) {
+      test.push("boop");
+      tests += "boop" + "</br>"
+
+   } else if (nestedArr[c].includes('0')) {
+      test.push("beep");
+      tests += "beep" + "</br>"
+    } else {
+      test.push(nestedArr[c].join(""));
+      tests += nestedArr[c].join("") + "</br>"
+    }
+   }
 
 
-document.getElementById("test").innerHTML = test;
-document.getElementById("test1").innerHTML = tests;
-document.getElementById("test2").innerHTML = nestedArr;
+  $("p#result").text(test);
+
+
+});
+});
