67bd851cf5c06d2e950e8c7dc20235ad81a3b87e
Eddie Harris <harrisejm@gmail.com>
2018-06-08 11:45:04 -0700

    git commit -m

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,9 @@ $(document).ready(function() {
 
   for (var c = 0; c < nestedArr.length; c++){
 
-   if (parseInt(nestedArr[c]) % 3 === 0 && parseInt(nestedArr[c]) !== 0) {
-      test.push("sorry");
-      tests += "sorry" + "</br>"
+   if (parseInt(nestedArr[c].join("").split(" ")) % 3 === 0 && parseInt(nestedArr[c].join("").split(" ")) !== 0) {
+      test.push("I'm sorry, Dave. I'm afraid I can't do that.");
+      tests += "I'm sorry, Dave. I'm afraid I can't do that." + "</br>"
    } else if (nestedArr[c].includes('1')) {
       test.push("boop");
       tests += "boop" + "</br>"
@@ -40,8 +40,8 @@ $(document).ready(function() {
    }
 
 
-  $("p#result").text(test);
-
+  //$("p#result").text(test);
+  document.getElementById("result").innerHTML = tests;
 
 });
 });
