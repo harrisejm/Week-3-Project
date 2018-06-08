8438b61459ac5cfa2f73f43856238cd11a4c6314
Eddie Harris <harrisejm@gmail.com>
2018-06-08 09:00:12 -0700

    add nested for array to check alldigits in each number

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,16 @@ for (var a = 0; a < numbArr.length; a++) {
   nestedArr.push(singleNumbArr);
 }
 
-for (var b = 0; b < nestedArr
+var test = [];
+
+for (var c = 0; c < nestedArr[c].length; c++){
+  for (var b = 0; b < nestedArr.length; b++) {
+    if ( )
+    test.push((nestedArr[b]).length);
+  }
+}
 
 
- document.getElementById("test").innerHTML = nestedArr[10];
+
+document.getElementById("test").innerHTML = test.length;
+ document.getElementById("test1").innerHTML = test;
