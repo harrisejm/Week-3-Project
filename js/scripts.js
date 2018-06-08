b76286f56820a27bc5c7a03b61a53f1bcd6638db
Eddie Harris <harrisejm@gmail.com>
2018-06-08 13:02:54 -0700

    add conditions to account for negative numbers, non-numbers and no entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,52 @@
 $(document).ready(function() {
+  var outPut = function(numb){
+     var numbArr = [];
+     var nestedArr = [];
+     //var outPutArr = [];
+     var outPutString = "";
+
+    if (isNaN(numb)) {
+      // outPutArr.push("Please enter real numbers only");
+       outPutString += "Please Enter REAL Numbers Only";
+    } else if (numb < 0) {
+    //  outPutArr.push("please enter positive numbers only");
+       outPutString = "Please Enter POSITIVE Numbers Only";
+    } else if (!numb) {
+    return // if you submit a blank field, treats it as a "0" and outputs beep.
+  }
+
+
+    for (var i = 0; i < numb+1; i++) {
+      numbArr.push(i.toString());
+    }
+    for (var a = 0; a < numbArr.length; a++) {
+      var singleNumb = numbArr[a];
+      var singleNumbArr = singleNumb.split("");
+      nestedArr.push(singleNumbArr);
+    }
+    for (var c = 0; c < nestedArr.length; c++){
+      if (parseInt(nestedArr[c].join("").split(" ")) % 3 === 0 && parseInt(nestedArr[c].join("").split(" ")) !== 0) {
+        //  outPutArr.push("I'm sorry, Dave. I'm afraid I can't do that.");
+        outPutString += "I'm sorry, Dave. I'm afraid I can't do that." + "</br>"
+      } else if (nestedArr[c].includes('1')) {
+        //  outPutArr.push("boop");
+        outPutString += "boop" + "</br>"
+      } else if (nestedArr[c].includes('0')) {
+        //   outPutArr.push("beep");
+        outPutString += "beep" + "</br>"
+      } else {
+        // outPutArr.push(nestedArr[c].join(""));
+        outPutString += nestedArr[c].join("") + "</br>"
+      }
+    }
+    //$("p#result").text(test);
+    //Used javaScript to output as a list that can be replaced
+    document.getElementById("result").innerHTML = outPutString;
+  }
   $("form#inputNumber").submit(function(event) {
     event.preventDefault();
+    var numb = $("input#number").val().trim();
+    outPut(numb);
+  });
 
-
-  var numb = parseInt($("input#number").val());
-  var numbArr = [];
-
-  for (var i = 0; i < numb+1; i++) {
-    numbArr.push(i.toString());
-  }
-
-  var nestedArr = [];
-
-  for (var a = 0; a < numbArr.length; a++) {
-    var singleNumb = numbArr[a];
-    var singleNumbArr = singleNumb.split("");
-    nestedArr.push(singleNumbArr);
-  }
-
-  var test = [];
-  var tests = "";
-
-  for (var c = 0; c < nestedArr.length; c++){
-
-   if (parseInt(nestedArr[c].join("").split(" ")) % 3 === 0 && parseInt(nestedArr[c].join("").split(" ")) !== 0) {
-      test.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      tests += "I'm sorry, Dave. I'm afraid I can't do that." + "</br>"
-   } else if (nestedArr[c].includes('1')) {
-      test.push("boop");
-      tests += "boop" + "</br>"
-
-   } else if (nestedArr[c].includes('0')) {
-      test.push("beep");
-      tests += "beep" + "</br>"
-    } else {
-      test.push(nestedArr[c].join(""));
-      tests += nestedArr[c].join("") + "</br>"
-    }
-   }
-
-
-  //$("p#result").text(test);
-  document.getElementById("result").innerHTML = tests;
-
-});
 });
